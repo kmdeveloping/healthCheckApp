d9295ecf81a512c137f6f5b24be1b357fa154e92
Kevin M <marinekev88@gmail.com>
2022-12-25 22:43:47 -0700

    wip

--- app/Jobs/TestJob1.cs
+++ app/Jobs/TestJob1.cs
@@ -7,7 +7,7 @@ namespace app.Jobs;
 public class TestJob1 : CronJobService
 {
   private readonly ILogger<TestJob1> _logger;
-  private ISlack _slack;
+  private readonly ISlack _slack;
 
   public TestJob1(ISchedulerConfig<TestJob1> config, ILogger<TestJob1> logger, ISlack slack) :
     base(config.CronExpression, config.TimeZoneInfo)
@@ -31,19 +31,7 @@ public class TestJob1 : CronJobService
 
   public override Task StopAsync(CancellationToken cancellationToken)
   {
-    _logger.LogInformation("Test job 1 is stopping.");
+    _logger.LogInformation("Test job 1 is stopping");
     return base.StopAsync(cancellationToken);
-  }
-
-  private List<int> CountToTen()
-  {
-    List<int> response = new();
-    var i = 0;
-    for (; i <= 100; i++)
-    {
-      response.Add(i);
-    }
-
-    return response;
   }
 }
